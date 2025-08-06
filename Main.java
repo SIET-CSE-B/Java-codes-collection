64a1dccbbb9f8bc99f46ebb9484c302bb576ca9d
HARISH R <harishpblr2007@gmail.com>
2025-08-06 11:12:07 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the string: ");
+        String str=sc.nextLine();
+        String rev="";
+        for(int i=str.length()-1;i>=0;i--){
+            rev+=str.charAt(i);
+        }
+        System.out.println("Reversed string is "+rev);
+    }
+}
